7c0dbb60963b81697f848d97b17c0518d190ba14
ChubbyQuokka <graybad1@icloud.com>
2018-03-31 04:29:23 -0400

    Organization

--- Rocket.Rust.Launcher/Program.cs
+++ Rocket.Rust.Launcher/Program.cs
@@ -1,9 +1,9 @@
 ﻿using System;
 using System.Diagnostics;
-using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Reflection;
+using System.Threading;
 
 using Mono.Cecil;
 using Mono.Cecil.Cil;
@@ -25,7 +25,7 @@ namespace Rocket.Rust.Launcher
                 foreach (Assembly assembly in assemblies)
                 {
                     AssemblyName name2 = assembly.GetName();
-                    if (string.Equals(name2.Name, assemblyName.Name, StringComparison.InvariantCultureIgnoreCase) && string.Equals(CultureToString(name2.CultureInfo), CultureToString(assemblyName.CultureInfo), StringComparison.InvariantCultureIgnoreCase))
+                    if (string.Equals(name2.Name, assemblyName.Name, StringComparison.InvariantCultureIgnoreCase) && string.Equals(name2.CultureInfo.Name ?? "", assemblyName.CultureInfo.Name ?? "", StringComparison.InvariantCultureIgnoreCase))
                     {
                         return assembly;
                     }
@@ -38,14 +38,13 @@ namespace Rocket.Rust.Launcher
                 {
                     return Assembly.LoadFile(file);
                 }
-                else if (File.Exists(fileSecondary))
+
+                if (File.Exists(fileSecondary))
                 {
                     return Assembly.LoadFile(fileSecondary);
                 }
-                else
-                {
-                    return null;
-                }
+
+                return null;
             };
         }
 
@@ -53,12 +52,11 @@ namespace Rocket.Rust.Launcher
         static void Main(string[] args)
         {
             byte[] originalRust = File.ReadAllBytes(Path.Combine(RustDir, "Assembly-CSharp.dll"));
-
             MemoryStream stream = new MemoryStream(originalRust);
+
             try
             {
                 AssemblyDefinition rust = AssemblyDefinition.ReadAssembly(stream);
-
                 MethodDefinition rustInit = rust.MainModule.Types.First(x => x.FullName.Equals("Bootstrap")).Methods.First(x => x.Name.Equals("Init_Systems"));
 
                 if (rustInit.Body.Instructions[0].OpCode != OpCodes.Call)
@@ -71,25 +69,34 @@ namespace Rocket.Rust.Launcher
                         //Add the strings to combine
                         processor.Create(OpCodes.Call, rustInit.Module.ImportReference(typeof(Directory).GetMethod("GetCurrentDirectory"))),
                         processor.Create(OpCodes.Ldstr, @"Rocket\Binaries\Rocket.Rust.dll"),
+
                         //Run Path.Combine
                         processor.Create(OpCodes.Call, rustInit.Module.ImportReference(typeof(Path).GetMethod("Combine", new Type[] { typeof(string), typeof(string)}))),
+
                         //Load the Assembly using the combined path
                         processor.Create(OpCodes.Call, rustInit.Module.ImportReference(typeof(Assembly).GetMethod("LoadFile", new Type[] { typeof(string) }))),
-                        //Load type for the reflection call
+
+                        //Load type name for the type resolution
                         processor.Create(OpCodes.Ldstr, "Rocket.Rust.Runtime.Startup"),
                         processor.Create(OpCodes.Callvirt, rustInit.Module.ImportReference(typeof(Assembly).GetMethod("GetType", new Type[]{ typeof(string) }))),
-                        //Load the name of the method to call
+
+                        //Load method name for the method resolation call
                         processor.Create(OpCodes.Ldstr, "Initialize"),
-                        //Load 24 onto the stack (BindingFlags.Static | BindingFlags.Public)
+
+                        //Load 24 for the method resolution (BindingFlags.Static | BindingFlags.Public)
                         processor.Create(OpCodes.Ldc_I4_S, (sbyte)24),
-                        //Load the target method onto the stack
+
+                        //Load the target method for the reflection call
                         processor.Create(OpCodes.Callvirt, rustInit.Module.ImportReference(typeof(Type).GetMethod("GetMethod", new Type[] { typeof(string), typeof(BindingFlags) }))),
-                        //Load two null values onto the stack for the invoke call Invoke(null, null)
+
+                        //Load two null values for the reflection call
                         processor.Create(OpCodes.Ldnull),
                         processor.Create(OpCodes.Ldnull),
-                        //Make this mother fucking reflection call
+
+                        //Make that fucking reflection call
                         processor.Create(OpCodes.Callvirt, rustInit.Module.ImportReference(typeof(MethodBase).GetMethod("Invoke", new Type[] { typeof(object), typeof(object[]) }))),
-                        //Get that ugly object reference of my stack that the stupid Invoke method returned. (100% null)
+
+                        //Get that ugly object reference out of my stack
                         processor.Create(OpCodes.Pop)
 
                     };
@@ -109,6 +116,7 @@ namespace Rocket.Rust.Launcher
             catch
             {
                 File.WriteAllBytes(Path.Combine(RustDir, "Assembly-CSharp.dll"), originalRust);
+                Console.WriteLine("An exception occured during patching, your instance of Assembly-CSharp.dll has been replaced with the original.");
                 throw;
             }
             finally
@@ -116,17 +124,13 @@ namespace Rocket.Rust.Launcher
                 stream.Dispose();
             }
 
-            Process.Start(Path.Combine(Directory.GetCurrentDirectory(), "RustDedicated.exe"), "-batchmode");
-        }
+            var newArgs = args.ToList();
+            newArgs.Add("-nographics");
 
-        static string CultureToString(CultureInfo culture)
-        {
-            if (culture == null)
-            {
-                return "";
-            }
+            Process.Start(Path.Combine(Directory.GetCurrentDirectory(), "RustDedicated.exe"), string.Join(" ", newArgs));
+            Console.WriteLine("Your Rust instance has been created, this window exit soon.");
 
-            return culture.Name;
+            Thread.Sleep(5000);
         }
     }
 }
